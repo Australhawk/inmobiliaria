232f3e845d007f8dbf0739ea70c435ece048a139
Sebastian <austral@gamerdeal.tv>
2013-07-06 17:05:25 -0400

    Agregada página de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Inmobiliaria::Application.routes.draw do
+  get "inicio/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
