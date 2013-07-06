5867655ef7590b33ee9918a8b51bb05bc42cfc5d
Sebastian <austral@gamerdeal.tv>
2013-07-06 17:02:09 -0400

    Instalación aplicación Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inmobiliaria::Application.config.secret_key_base = '95054b99cbbea9a5f47c94a012c4beb3b908ec4a2803eb6fca5f6146d7c3b7d4e925c000447c2f0b7aaa81c41a35f2709f51d676ba1e2880f45319ce928b2aaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inmobiliaria::Application.config.session_store :cookie_store, key: '_Inmobiliaria_session'
