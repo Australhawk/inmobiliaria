c153770d3229a6b4fe32786cf33e4a59d8bb0e44
Sebastian <austral@gamerdeal.tv>
2013-07-06 17:11:31 -0400

    Cambiados css a SCSS y Agregado Root Path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Inmobiliaria::Application.routes.draw do
   get "inicio/index"
+  root "inicio#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
